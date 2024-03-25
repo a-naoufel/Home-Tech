3ba793215db7e474ed9a96406acee5bba0d5449c
a-naoufel <n_azizi@estin.com>
2024-03-25 15:40:16 +0100

    the primary api with the product model, the base app (sign up ,log in ) by Yakoub integration of the base app with the product model by Naoufel

--- HomeTech/HomeTech/models.py
+++ HomeTech/HomeTech/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)    
+    description = models.TextField()    
+    image = models.ImageField(upload_to='products/', null=True, blank=True) 
+    
     

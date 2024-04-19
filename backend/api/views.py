2d64626571337cb486fd5b0121f3f0582281d366
Yakoubbouzit <yakoubbouzit06@gmail.com>
2024-04-20 00:50:30 +0100

    push as naoufel said me after meetting

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import UserSerializer, ProductSerializer, CartSerializer
 from .models import Product, Cart
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-from django.http import JsonResponse
 
 
 # Create your views here.
@@ -28,7 +23,6 @@ class ProductList(generics.ListCreateAPIView):
     def get_queryset(self):
         return Product.objects.all()
     
-
     def perform_create(self, serializer):
         if serializer.is_valid():
             serializer.save(author=self.request.user)
@@ -57,16 +51,7 @@ class ProductDetail(generics.ListAPIView):
     queryset = Product.objects.all
     
     
-class MyProductDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
-    def get_queryset(self):
-        return Product.objects.filter(author=self.request.user)
-    def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
-        else:
-            print(serializer.errors)
+
 
 class CartList(generics.ListCreateAPIView):
     serializer_class = CartSerializer

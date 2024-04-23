43d1451ae80ee39bb1887c548b8cd8752c8ad594
a-naoufel <n_azizi@estin.com>
2024-04-23 15:55:45 +0100

    login and sing up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,47 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login-leader/Login";
 
 import Home from "./pages/Home/Home";
+import Page from "./pages/Login/login"
+import Regester from "./pages/Regester/regester"
 import Header from "./Components/header/Header";
 import Footer from "./Components/footer/Footer";
-import Register from "./pages/Regester-leader/Register";
 import AddProduct from "./pages/AddProduct/AddProduct";
 import NotFound from "./pages/Not-Found/NotFound";
 import { Shop } from "./pages/shopPage/Shop";
 import ProtectedRoute from "./Components/protectedroot/ProtectedRoute";
 function Logout() {
-  localStorage.clear()
-  return <Navigate to="/login" />
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
 function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
+  localStorage.clear();
+  return <Regester />;
 }
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Header />
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/register" element={<RegisterAndLogout />} />
           <Route path="/home" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
-          <Route path="/add-product" element={ <ProtectedRoute>
-                                              <AddProduct />
-                                            </ProtectedRoute>} />
-        
-          
+          <Route path="/login" element={<Page />} />
+
+
+          <Route
+            path="/add-product"
+            element={
+              <ProtectedRoute>
+                <AddProduct />
+              </ProtectedRoute>
+            }
+          />
+
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
         <Footer />

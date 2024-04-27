5306b589631635918f190dc380c5b90e27690ee4
a-naoufel <n_azizi@estin.com>
2024-04-27 09:57:14 +0100

    allowany one to see product_

--- backend/api/views/product_views.py
+++ backend/api/views/product_views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -12,6 +12,7 @@ from rest_framework import status
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def getProducts(request):
     query = request.query_params.get('keyword')
     if query == None:

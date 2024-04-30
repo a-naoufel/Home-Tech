7d65450840085345f88d071d410870d54b0495a3
a-naoufel <n_azizi@estin.com>
2024-04-30 17:02:38 +0100

    create some profils

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns = format_suffix_patterns(urlpatterns) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

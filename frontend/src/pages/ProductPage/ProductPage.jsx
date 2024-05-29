3de3572bf93dbbe6496aa8ca395012e73425f163
a-naoufel <n_azizi@estin.com>
2024-05-29 19:40:04 +0100

    Product page to style

--- frontend/src/pages/ProductPage/ProductPage.jsx
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams, useNavigate } from "react-router-dom";
+
 import {
   Row,
   Col,
@@ -44,13 +45,82 @@ const ProductScreen = () => {
         navigate("/*")
       ) : (
         <div>
-          <img
-            src={product.image}
-            width={500}
-            height={500}
-            alt={product.name}
-          />
-          {product.name}
+
+          <Row>
+            <Col md={6}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
+
+            <Col md={3}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  {/* Add your custom styling here */}
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  Price: ${product.price}
+                </ListGroup.Item>
+
+                <ListGroup.Item>
+                  Description: {product.description}
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+
+            <Col md={3}>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col xs='auto' className='my-1'>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                          >
+                            {
+                              [...Array(product.countInStock).keys()].map((x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              ))
+                            }
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  <ListGroup.Item>
+                    {/* Add your custom styling here */}
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
         </div>
       )}
     </div>

8e5f67bfe731b9bb2e153c739a09d685e67c56c1
a-naoufel <n_azizi@estin.com>
2024-05-30 22:42:57 +0100

    user parofile page to be styled

--- frontend/src/pages/ProfilePage/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,79 +1,109 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingIndicator from "../../Components/LoadingIndicator/LoadingIndicator";
 import Message from "../../Components/Message";
-import { getUserDetails,updateUser } from "../../../../frontend1vite/src/actions/userActions";
+import {
+  getUserDetails,
+  updateUserProfile,
+} from "../../../../frontend1vite/src/actions/userActions";
 import { USER_UPDATE_PROFILE_RESET } from "../../../../frontend1vite/src/constants/userConstants";
-import { listMyOrders } from '../../actions/orderActions'
-import {useNavigate} from "react-router-dom"
+import { listMyOrders } from "../../actions/orderActions";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [message, setMessage] = useState('');
-    
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    const userDetails = useSelector(state => state.userDetails)
-    const { error, loading, user } = userDetails
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const userLogin = useSelector(state => state.userLogin)
-    const { userInfo } = userLogin
+  const userDetails = useSelector((state) => state.userDetails);
+  const { error, loading, user } = userDetails;
 
-    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
-    const { success } = userUpdateProfile
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
-    const orderListMy = useSelector(state => state.orderListMy)
-    const { loading: loadingOrders, error: errorOrders, orders } = orderListMy
-    
-    useEffect(() => {
-        if (!userInfo) {
-            navigate('/login')
-        } else {
-            if (!user || !user.name || success || userInfo._id !== user._id) {
-                dispatch({ type: USER_UPDATE_PROFILE_RESET })
-                dispatch(getUserDetails('profile'))
-                dispatch(listMyOrders())
-            } else {
-                setName(user.name)
-                setEmail(user.email)
-            }
-        }
-    }, [dispatch, navigate, userInfo, success])
-    console.log(user)
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
+  const { success } = userUpdateProfile;
 
-    const submitHandler = (e) => {
-        e.preventDefault()
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy;
 
-        if (password != confirmPassword) {
-            setMessage('Passwords do not match')
-        } else {
-            dispatch(updateUserProfile({
-                'id': user._id,
-                'name': name,
-                'email': email,
-                'password': password
-            }))
-            setMessage('')
-        }
-
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    } else {
+      if (!user || !user.name || success || userInfo._id !== user._id) {
+        dispatch({ type: USER_UPDATE_PROFILE_RESET });
+        dispatch(getUserDetails("profile"));
+        dispatch(listMyOrders());
+      } else {
+        setName(user.name);
+        setEmail(user.email);
+      }
     }
+  }, [dispatch, navigate, userInfo, success]);
+  console.log(user);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
 
-    return (
-        <div>
-            <h1>Profile Page</h1>
-            <h1>{user.username}</h1>
-            {message && <Message variant='danger'>{message}</Message>}
-                {error && <Message variant='danger'>{error}</Message>}
-                {loading && <LoadingIndicator />}
-            
+    if (password != confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      dispatch(
+        updateUserProfile({
+          id: user._id,
+          name: name,
+          email: email,
+          password: password,
+        })
+      );
+      setMessage("");
+    }
+  };
 
-        </div>
-    )
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <h1>{user.username}</h1>
+      {message && <Message variant="danger">{message}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <LoadingIndicator />}
+      <form onSubmit={submitHandler}>
+        <label>Name</label>
+        <input
+          type="text"
+          value={name}
+          placeholder={user.name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          placeholder={user.username}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        ></input>
+        <button type="submit">Update</button>
+      </form>
+    </div>
+  );
 }
 
 export default ProfilePage;
